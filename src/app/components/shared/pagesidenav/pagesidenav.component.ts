5c9131085c8ec9d7b2bd4b1fe441d7e016b72637
Mehedi-Hassan <mehedikazi812@gmail.com>
2024-07-28 19:33:14 +0600

    add temporary interface for NavigateItem of sidenav and created variable of interface and initial that in constructor

--- src/app/components/shared/pagesidenav/pagesidenav.component.ts
+++ src/app/components/shared/pagesidenav/pagesidenav.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+// interface for navigate item
+export interface NavigateItem{
+  value: string;
+  link: string;
+} 
+
 @Component({
   selector: 'pagesidenav',
   templateUrl: './pagesidenav.component.html',
@@ -7,6 +13,19 @@ import { Component } from '@angular/core';
 })
 export class PagesidenavComponent {
 
+  //this is the panel name
   panelName: string = 'Student Panel';
 
+  // variable of NavigateItem
+  navItem: NavigateItem[] = [];
+
+  constructor(){
+    // initialize the navItem
+    this.navItem = [
+      {value:"View books",link:'view-books'},
+      {value:'My Orders', link:'my-orders'}
+    ]
+  }
+
 }
+ 
